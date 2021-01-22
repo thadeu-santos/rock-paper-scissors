5281c75ee258cef52de1a6585613c5becfdb0ea4
thadeu-santos <thadeulsantos@gmail.com>
2021-01-21 23:24:54 -0300

    segunda etapa funcionalidade do jogo

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice () {
+    const choices = ["r", "p", "s"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
 function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    
 
 }
 
